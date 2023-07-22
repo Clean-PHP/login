fd4e71328bb144fe298eb2b73e76435711c1dd71
ankio <admin@ankio.net>
2023-07-22 14:24:01 +0800

    fix:修复登录API

--- AnkioApi.php
+++ AnkioApi.php
@@ -18,26 +18,18 @@ use cleanphp\base\Config;
 use cleanphp\base\Json;
 use cleanphp\base\Request;
 use cleanphp\file\Log;
-use library\encryption\AESEncrypt;
 use library\http\HttpClient;
 use library\http\HttpException;
 
 class AnkioApi
 {
     private static ?AnkioApi $instance = null;
-    public string $url = "";
-    public string $appId = "";
-    public string $secretKey = "";
 
+    public SsoConfig $config;
 
     public function __construct()
     {
-        $config = Config::getConfig('sso');
-        if (!empty($config)) {
-            $this->url = $config['url'];
-            $this->appId = $config['appId'];
-            $this->secretKey = $config['secretKey'];
-        }
+        $this->config = new SsoConfig(Config::getConfig("notice"));
     }
 
     static function getInstance(): ?AnkioApi
@@ -56,10 +48,8 @@ class AnkioApi
                 'User-Agent' => Request::getHeaderValue('User-Agent') ?? 'NO UA'
             ];
             $data['t'] = time();
-            $data['appid'] = $this->appId;
-            Log::record('SignUtils', json_encode($data));
-            $response = HttpClient::init($this->url)->setHeaders($headers)->post(SignUtils::sign($data, $this->secretKey), 'form')->send($url);
-            Log::record('API', $response->getBody());
+            $data['appid'] = $this->config->id;
+            $response = HttpClient::init( $this->config->url)->setHeaders($headers)->post(SignUtils::sign($data,  $this->config->key), 'form')->send($url);
             return Json::decode($response->getBody(), true);
         } catch (HttpException $e) {
             Log::record("API", $e->getMessage(), Log::TYPE_ERROR);
@@ -75,11 +65,9 @@ class AnkioApi
      * @param string $fromname
      * @return mixed|true
      */
-    static function sendMail(string $mailto, string $subject, string $content, string $fromname)
+    static function sendMail(string $mailto, string $subject, string $content, string $fromname): mixed
     {
-        $aes = new AESEncrypt(self::getInstance()->secretKey);
-        $data = self::getInstance()->request("api/mail/send", ["mailto" => $mailto, "subject" => $subject, "fromname" => $fromname, "content" => $aes->encryptWithOpenssl($content)]);
-        Log::record("Mail", json_encode($data, JSON_UNESCAPED_UNICODE));
+        $data = self::getInstance()->request("api/mail/send", ["mailto" => $mailto, "subject" => $subject, "fromname" => $fromname, "content" => base64_encode($content)]);
         if ($data["code"] == 200) return true;
         return $data["msg"];
     }
